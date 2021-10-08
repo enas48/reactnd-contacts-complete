cf859562afb964956d65d91412945acb67935426
enas48 <enasm4829@gmail.com>
2021-10-08 16:10:30 +0200

    addedd contact

---
+++ src/ListContacts.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class ListContacts extends React.Component {
+    render() { 
+     
+        return <ol className="contact-list">
+            {this.props.contacts.map((item, index)=>
+            <li key={index} className="contact-list-item">
+                <div className="contact-avater" style={{backgroundImage:`url(${item.avaterURL})`}}></div>
+                <div className="contact-details">
+                    <p>{item.name}</p>
+                    <p>{item.email}</p>
+                    <button className="contact-remove">Remove</button>
+                </div>
+            </li>)}
+        </ol>;
+    }
+}
+ 
+export default ListContacts;
