159a565b6fc3d19d27f6dd691246b443c1d2da5a
enas48 <enasm4829@gmail.com>
2021-10-09 15:37:18 +0200

    fix image -

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,20 +1,19 @@
 import React from 'react';
 
-class ListContacts extends React.Component {
-    render() { 
+function ListContacts(props)  {
      
         return <ol className="contact-list">
-            {this.props.contacts.map((item, index)=>
-            <li key={index} className="contact-list-item">
-                <div className="contact-avatar" style={{backgroundImage:`url(${item.avatarURL})`}}></div>
+            {props.contacts.map((contact)=>
+            <li key={contact.id} className="contact-list-item">
+                <div className="contact-avatar" style={{backgroundImage:`url(${contact.avatarURL})`}}></div>
                 <div className="contact-details">
-                    <p>{item.name}</p>
-                    <p>{item.email}</p>
-                    <button className="contact-remove">Remove</button>
+                    <p>{contact.name}</p>
+                    <p>{contact.email}</p>
+                    <button className="contact-remove"  onClick={() => props.onDeleteContact(contact)} >Remove</button>
                 </div>
             </li>)}
         </ol>;
-    }
+ 
 }
  
 export default ListContacts;
